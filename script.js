4a14cf2b0f093ae7d8fc9fcd2b05af0e2c480c54
Ananas <mrs.anna.noga@gmail.com>
2023-04-03 01:15:52 +0100

    changed outfit of webpage

--- script.js
+++ script.js
@@ -3,11 +3,54 @@ const burger = document.querySelector('nav svg');
 burger.addEventListener("click", () => {
     if (burger.classList.contains("active")){
         gsap.to(".links", {x: "100%"});
-        gsap.to(".line", {stroke: "white"})
+        gsap.to(".line", {stroke: "white"});
+        gsap.set("html", {overflow: "auto"});
+        gsap.set("html", {overflowX: "hidden"});
     } else{
         gsap.to(".links", {x: "0%"});
         gsap.to(".line", {stroke: "black"})
-
+        gsap.fromTo(".links a", 
+        {opacity:0, y:0},
+        {opacity:1, y:20, delay:0.25, stagger:0.15 });
+        gsap.set("html", {overflow: "hidden"});
     }
     burger.classList.toggle("active")
-})
+});
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, {opacity: 0});
+
+videos.forEach((video) => {
+    ScrollTrigger.create({
+        trigger: video,
+        start: "top center",
+        end: "bottom center",
+        markers: true,
+        onEnter: () => {
+            gsap.to(video, {opacity: 1});
+            video.play();
+        },
+        onEnterBack: () => video.play(),
+        onLeave: () => video.pause(),
+        onLeaveBack: () => video.pause(),
+    });
+});
+
+
+const exploreBtn = document.getElementsByClassName('cta-sec');
+const contactBtn = document.getElementsByClassName('cta-main');
+
+
+exploreBtn.addEventListener('click', () => {
+  
+    const servicesSection = document.getElementsByClassName('classes');
+
+
+  const servicesOffset = servicesSection.offsetTop;
+
+  window.scrollTo({
+    top: servicesOffset,
+    behavior: 'smooth'
+  });
+  });
+  
